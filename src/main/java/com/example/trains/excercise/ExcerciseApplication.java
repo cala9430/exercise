01069abba5e61fb40ce91061a8e1f90c53fb4ae3
Daniel Calabuig <calabuig.daniel@gmail.com>
2019-06-11 22:40:13 -0300

    Initial commit

---
+++ src/main/java/com/example/trains/excercise/ExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.trains.excercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trains/excercise/ExcerciseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.trains.excercise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExcerciseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
